010925abb6bad42e8de0445cd449b42cec12bdcc
Annushka-chauhan <anushkachauhanannu@gmail.com>
2026-06-23 17:01:47 +0530

    Modified Files

--- NOTES_APP/index.js
+++ NOTES_APP/index.js
@@ -1,5 +1,7 @@
 const express= require("express");
+const jwt = require("jsonwebtoken")
 const app = express();
+
 //if there is any body in the req this is the middleware which extracts it
 app.use(express.json());
 //POST = Create a note
@@ -8,13 +10,13 @@ const users=[];
 app.post("/signup",function(req,res){
     const username = req.body.username;
     const password = req.body.password;
-    const userExist= users.find(user => user.usernmae === username)
+    const userExist= users.find(user => user.username === username)
     if(userExist){
         return res.status(403).json({
            message : "User with this name already exist "
         })
     }
-    user.push({
+    users.push({
         username: username,
         password: password
     })
@@ -22,6 +24,30 @@ app.post("/signup",function(req,res){
         message: "You have signed up "
     })
 })
+
+app.post("/signin", function(req,res){
+    const username = req.body.username;
+    const password= req.body.username;
+    const userExist = users.find(user=>user.username === username && user.password === password)
+    if(!userExist){
+        response.status(403).json({
+            message: "InCorrect Credentials"
+        })
+        return;
+    }
+    //json web token 
+   const token = jwt.sign({
+    username: username
+   },"Annu123")
+   res.json({
+    token: token
+   })
+})
+
+//json web Tokem 
+
+
+
 app.post("/notes",function (req,res){
     const note = req.body.note;
     notes.push(note);
