f9687279abc48b54e45284ae1c85b71f7d43195e
Annushka-chauhan <anushkachauhanannu@gmail.com>
2026-06-24 12:32:03 +0530

    New Learning

---
+++ NOTES_APP/middleware.js
@@ -0,0 +1,21 @@
+function auth(req, res, next) {
+    const token = req.headers.token;
+
+    if (!token) {
+        return res.status(403).json({
+            message: "You are not logged in"
+        });
+    }
+
+    try {
+        const decoded = jwt.verify(token, "Annu123");
+
+        req.username = decoded.username;
+
+        next();
+    } catch (err) {
+        return res.status(403).json({
+            message: "Invalid token"
+        });
+    }
+}
